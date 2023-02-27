07df18eec6e7605b6837dbcc9eeaab26c640ea58
sam caldwell <mail@samcaldwell.net>
2023-02-26 22:51:57 -0600

    continue working ont he entrypint.sh script

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -4,6 +4,12 @@
 # Docker registry entrypoint
 #
 
+#
+# our symlink will be used to ensure everything that
+# consumes the configuration will get the same config
+#   - registry serve (this file)
+#   - registry garbage-collect (daily cron)
+#
 if [[ "${ENVIRONMENT}" == "development" ]]; then
   ln -sf /etc/docker/registry/config-dev.yml /etc/docker/registry/config.yml
 else
@@ -13,6 +19,8 @@ fi
 if [[ "${REGISTRY_HTTP_SECRET}_" == "_" ]]; then
   echo "you must set REGISTRY_HTTP_SECRET before running"
   exit 1
-else
-  registry serve /etc/docker/registry/config.yml
 fi
+
+service cron start
+
+registry serve /etc/docker/registry/config.yml
