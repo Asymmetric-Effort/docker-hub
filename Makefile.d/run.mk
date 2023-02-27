ea3da532deaa4db063addca3877af33dc9dddd0d
sam caldwell <mail@samcaldwell.net>
2023-02-26 22:52:25 -0600

    evolving run.mk

--- Makefile.d/run.mk
+++ Makefile.d/run.mk
@@ -13,9 +13,9 @@ docker_hub/run: docker_hub/initialize
 	@docker run -d \
 			   --rm \
 			   --name $(DOCKER_HUB_DOCKER_CONTAINER_NAME) \
-			   --publish $(DOCKER_HUB_IP_ADDRESS):$(DOCKER_HUB_SSH_PORT):22 \
-			   --publish $(DOCKER_HUB_IP_ADDRESS):$(DOCKER_HUB_HTTP_PORT):80 \
-			   --volume $(DOCKER_HUB_VOLUME):/git/ $(REMOTE_IMAGE) .
+			   --publish $(DOCKER_HUB_IP_ADDRESS):$(DOCKER_HUB_PORT):5000 \
+			   --e REGISTRY_HTTP_SECRET=$(DOCKER_HUB_REGISTRY_HTTP_SECRET) \
+			   --volume $(DOCKER_HUB_VOLUME):/var/lib/registry/ $(REMOTE_IMAGE) .
 	@docker ps
 	@echo "$@ completed."
 
@@ -29,8 +29,8 @@ docker_hub/run/local: docker_hub/initialize
 	@docker run -d \
 			   --rm \
 			   --name $(DOCKER_HUB_DOCKER_CONTAINER_NAME) \
-			   --publish $(DOCKER_HUB_IP_ADDRESS):$(DOCKER_HUB_SSH_PORT):22 \
-			   --publish $(DOCKER_HUB_IP_ADDRESS):$(DOCKER_HUB_HTTP_PORT):80 \
-			   --volume $(DOCKER_HUB_VOLUME):/git/ $(LOCAL_IMAGE) .
+			   --publish $(DOCKER_HUB_IP_ADDRESS):$(DOCKER_HUB_PORT):5000 \
+			   --e REGISTRY_HTTP_SECRET=$(DOCKER_HUB_REGISTRY_HTTP_SECRET) \
+			   --volume $(DOCKER_HUB_VOLUME):/var/lib/registry/ $(LOCAL_IMAGE) .
 	@docker ps
 	@echo "$@ completed."
